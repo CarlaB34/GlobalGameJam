fdaa3261247069169f7e87604d7fbe0040cc81c2
TiffaineP <62009859+TiffaineP@users.noreply.github.com>
2021-01-30 06:06:41 +0100

    Scene test de camera

---
+++ Assets/Tiffaine/SC_Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Movement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+
+    public float turnsmoothtime = 0.1f;
+    float turnsmoothvelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnsmoothvelocity, turnsmoothtime);
+
+            controller.Move(direction * speed * Time.deltaTime);
+
+        }
+
+
+    }
+}
